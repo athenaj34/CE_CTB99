9d39a69108714738e60686da9dc96cb6300d0c35
minerva <75211435+nervaishere@users.noreply.github.com>
2021-05-16 14:04:29 +0430

    add course removing

--- Naeimi/Students_registration.cpp
+++ Naeimi/Students_registration.cpp
@@ -1,12 +1,38 @@
 //
 // Created by athena on 8/5/21.
-// + taking courses
+// +taking courses +course removation
 #include <iostream>
 #include <vector>
 using namespace std;
+class subject{
+    string name_,teacher_name_,day_;
+    int time_h_,time_min_,coefficient_;
+public:
+    subject(string name,string teacher_name,string day,int time_h,int time_min,int coefficient):name_(name),teacher_name_(teacher_name),day_(day),time_h_(time_h),time_min_(time_min),coefficient_(coefficient){
+
+    }
+    string return_Sname()const{
+        return name_;
+    }
+    string return_STname()const{
+        return teacher_name_;
+    }
+    string return_day()const{
+        return day_;
+    }
+    int return_time_h()const{
+        return time_h_;
+    }
+    int return_time_m()const{
+        return time_min_;
+    }
+    int return_coefficient()const{
+        return coefficient_;
+    }
+};
 class student{
     string name_,lastname_,student_number_,password_;
-    vector<string>subjects;
+    vector<subject>subjects;
 public:
     student(string name,string lastname,string student_number,string password):name_(name),lastname_(lastname),student_number_(student_number),password_(password){
 
@@ -23,28 +49,35 @@ public:
     string return_lastname()const{
         return lastname_;
     }
-    vector<string> return_subjects(){
+    vector<subject> return_subjects(){
         return subjects;
     }
 };
-vector<string>subjects_list(){
-    vector<string> subject_;
-    subject_.push_back("Advanced Programming");
-    subject_.push_back("Calculus2");
-    subject_.push_back("Basic Programming");
-    subject_.push_back("Physics2");
-    subject_.push_back("Physics1");
-    subject_.push_back("Calculus1");
-    return subject_;
+
+vector<subject>subjects_list(vector<subject>subjects){
+    subjects.push_back(subject("Advanced Programming","Abolfazl Naeimi","Saturday",9,55,3));
+    subjects.push_back(subject("Calculus2","Behrouz Daneshian","Monday",10,55,3));
+    subjects.push_back(subject("Advanced Programming","Abolfazl Naeimi","Saturday",13,30,3));
+    subjects.push_back(subject("Basic Programming","Abolfazl Naeimi","Monday",9,55,3));
+    subjects.push_back(subject("Basic Programming","Maryam Afshari","Sunday",9,55,3));
+    subjects.push_back(subject("Claculus-1","Morteza Ghorbani","Saturday",9,55,3));
+    subjects.push_back(subject("Calculus-1","Mansour Ebrahimi","Wednesday",9,55,3));
+    subjects.push_back(subject("RE","Nafise Khansari","Saturday",9,10,2));
+    subjects.push_back(subject("RE","Siavash Bahrami","Monday",15,10,2));
+    subjects.push_back(subject("Physics-1","Farshid Nouralishahi","Saturday",9,55,3));
+    subjects.push_back(subject("Physics-1","Samira Zolghadr","Monday",9,30,3));
+    return subjects;
 }
 int main(){
     char command;
     vector <student> students;
+    vector <subject> subjects;
+    subjects=subjects_list(subjects);
     cout << "------------------------------------\n";
     cout << "1)Sign up \t\t2)Sign in\t\t 3)Exit \n";
     string name, lastname, student_number, password;
     while(cin >> command) { ;
-        if (command == '1') { //making obj
+        if (command == '1') {
 
             cout << "Enter your name: ";
             cin.get();
@@ -78,40 +111,90 @@ int main(){
                     student_number == students[i].return_student_number() &&
                     password == students[i].return_password()) {
                     cout << "****Login successfully****\n";
-                    vector<string> sub =students[i].return_subjects();
-                    vector<string> sub_list=subjects_list();
-                    if(sub.size()==0) {
-                        cout << "Choose at least 3 subjects from below:(after each number press enter)\n";
-                        for (int i = 0; i < sub_list.size(); ++i)
-                            cout << i + 1 << ") " << sub_list[i] << "   ";
+                    cout << "**************************\n**************************\n**************************\n";
+                    vector<subject> sub =students[i].return_subjects();
+                    if(sub.empty()) {
+                        for (int i = 0; i < subjects.size(); ++i)
+                            cout << i + 1 << ") " << subjects[i].return_Sname() << " // " << subjects[i].return_STname() <<" // " << subjects[i].return_day() << " // " << subjects[i].return_time_h() << ":" << subjects[i].return_time_m() << "\t|" << subjects[i].return_coefficient() << endl ;
+                        cout << "Choose courses from the courses list:(after each number press enter)\n";
                         cout << endl;
                         int k = 0;
                         int num;
+                        int coef=0;
                         char esc;
                         while (true) {
-                            k++;
-                            cin >> num;
-                            sub.push_back(sub_list[num - 1]);
-                            if (k >= 3) {
-                                cout << "Type \'e' to exit \n or press any key to continue then enter subject number\n";
+                            if (coef <=20) {
+                                cout << "Enter the course number:\n";
+                                k++;
+                                cin >> num;
+                                sub.push_back(subjects[num - 1]);
+                                coef += subjects[num - 1].return_coefficient();
+                                starting_menu:
+                                cout << "  coefficient till now: " << coef << endl;
+                                cout << "Type \'e' to exit \nType \'u' to edit your choices\npress any key to continue then enter course number\n";
                                 cin >> esc;
-                                if (esc=='e' || k>=6)
+                                if (esc == 'e' || k >= subjects.size())
                                     break;
-                                else if(esc=='\n')
+                                else if (esc == 'u') {
+                                    editing_menu:
+                                    while (true) {
+                                        cout << "------------------------------------\n";
+                                        cout << "Your subjects are:\n";
+                                        for (int f = 0; f < sub.size(); f++)
+                                            cout << f + 1 << ") " << sub[f].return_Sname() << " // "
+                                                 << sub[f].return_STname() << " // " << sub[f].return_day() << " // "
+                                                 << sub[f].return_time_h() << ":" << sub[f].return_time_m() << "\t|"
+                                                 << sub[f].return_coefficient() << endl;
+                                        cout << endl;
+                                        cout << "To delete a course enter its number\n";
+
+                                        cin >> num;
+                                        cout << sub[num - 1].return_Sname() << " // " << sub[num - 1].return_STname()
+                                             << " // " << sub[num - 1].return_day() << " // "
+                                             << sub[num - 1].return_time_h() << ":" << sub[num - 1].return_time_m()
+                                             << "\t|" << sub[num - 1].return_coefficient();
+                                        sub.erase(sub.begin() + (num - 1));
+                                        cout << "    Successfully deleted\n";
+                                        cout << "------------------------------------\n";
+                                        again:
+                                        cout << "Type \'e' to exit editing menu \nType \'u' to continue editing \n";
+                                        cin >> esc;
+                                        if (esc == 'e')
+                                            goto starting_menu;
+                                        else if (esc == 'u')
+                                            continue;
+                                        else {
+                                            cout << "unsupported input try again\n";
+                                            goto again;
+                                        }
+                                    }
+                                }
+                                else
                                     continue;
+
+                            }
+                            if(coef>20){
+                                cout << "you've reached the limit\n";
+                                cout << "Type \'e' to exit\nType \'u' to enter to editing menu\n";
+                                cin >> esc;
+                                if(esc=='u')
+                                    goto editing_menu;
+                                else if(esc=='e')
+                                    break;
                             }
                         }
                     }
                     cout << "------------------------------------\n";
                     cout << "Your subjects are:\n";
                     for(int f=0; f<sub.size();f++)
-                        cout << "  " << sub[f] ;
+                        cout << "  " << sub[f].return_Sname() << " // " << sub[f].return_STname() <<" // "<< sub[f].return_day() << " // " << sub[f].return_time_h() << ":" << sub[f].return_time_m() << "\t|" << sub[f].return_coefficient() << endl ;
                     cout << endl;
                     cout << "------------------------------------\n";
                 }
-                else if (password != students[i].return_password() ||
+                else if ((password != students[i].return_password() ||
                          student_number != students[i].return_student_number() ||
-                         lastname != students[i].return_lastname() || name != students[i].return_name() && (i!=students.size()-1) ){
+                         lastname != students[i].return_lastname() || name != students[i].return_name()) && (i!=students.size()-1) ){
+                    cout << i << endl << students.size()-1 << endl;
                     continue;
                 }
                 else    {
