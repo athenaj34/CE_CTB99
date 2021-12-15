3a4bdc6a3278fc2ab8df2170b2585b04b0347ea3
nervaishere <nervaishere@github.com>
2021-12-15 19:22:49 +0330

    update Project2/

---
+++ DS_Vaziri/Project2/Node.h
@@ -0,0 +1,27 @@
+#ifndef PROJECT2_Node_H
+#define PROJECT2_Node_H
+
+
+class Node {
+private:
+	char letter;
+	Node *next;
+public:
+	Node();
+	Node(char Letter);
+	int Length(Node *&start);
+	int Search(char letter, Node *start);
+	void Delete(int index, Node *& start);
+	void Add(int index, Node *&start, Node *new_element);
+	void UPPERCASE(Node *&start);
+	void lowercase(Node *&start);
+	void Print(Node *&start);
+	void reversedPrint(Node *&start);
+	char return_let();
+	Node* return_next();
+	void fill_let(char letter);
+	void fill_next(Node* next);
+};
+
+
+#endif //PROJECT2_Node_H
