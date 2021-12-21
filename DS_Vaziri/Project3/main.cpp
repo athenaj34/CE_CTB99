261d43d40054a547ffa4974f2e9293dd9885cdad
nervaishere <nervaishere@github.com>
2021-12-21 05:28:01 +0330

    update Project3/

--- DS_Vaziri/Project3/main.cpp
+++ DS_Vaziri/Project3/main.cpp
@@ -7,81 +7,59 @@ using namespace std;
 #define N 4
 #define M 5
 int n = N, m = M, fx, fy;
-
 bool visited[N][M];
 
 bool isReachable(int maze[N][M]) {
 	int i = 0, j = 0;
-	
-	stack<Node> s;
-	
+	stack<Node> path;
 	Node temp(i, j);
-	
-	s.push(temp);
-
-	while (!s.empty()) {
-
-		temp = s.top();
+	path.push(temp);
+	while (!path.empty()) {
+		temp = path.top();
 		int d = temp.return_dir();
 		i = temp.return_x(), j = temp.return_y();
-
-
 		temp.dir_incre();
-		s.pop();
-		s.push(temp);
-
-		if (i == fx and j == fy) {
-			return true;
-		}
-
+		path.pop();
+		path.push(temp);
+		if (i == fx && j == fy)	return true;
 		if (d == 0) {
-			if (i - 1 >= 0 and maze[i - 1][j] and
-									visited[i - 1][j]) {
+			if (i - 1 >= 0 && maze[i - 1][j] && visited[i - 1][j]) {
 				Node temp1(i - 1, j);
 				visited[i - 1][j] = false;
-				s.push(temp1);
+				path.push(temp1);
 			}
 		}
-
 		else if (d == 1) {
-			if (j - 1 >= 0 and maze[i][j - 1] and
-									visited[i][j - 1]) {
+			if (j - 1 >= 0 && maze[i][j - 1] && visited[i][j - 1]) {
 				Node temp1(i, j - 1);
 				visited[i][j - 1] = false;
-				s.push(temp1);
+				path.push(temp1);
 			}
 		}
-
 		else if (d == 2) {
-			if (i + 1 < n and maze[i + 1][j] and
-									visited[i + 1][j]) {
+			if (i + 1 < n && maze[i + 1][j] && visited[i + 1][j]) {
 				Node temp1(i + 1, j);
 				visited[i + 1][j] = false;
-				s.push(temp1);
+				path.push(temp1);
 			}
 		}
 		else if (d == 3) {
-			if (j + 1 < m and maze[i][j + 1] and
-									visited[i][j + 1]) {
+			if (j + 1 < m && maze[i][j + 1] && visited[i][j + 1]) {
 				Node temp1(i, j + 1);
 				visited[i][j + 1] = false;
-				s.push(temp1);
+				path.push(temp1);
 			}
 		}
-
-
 		else {
 			visited[temp.return_x()][temp.return_y()] = true;
-			s.pop();
+			path.pop();
 		}
 	}
-
 	return false;
 }
 
 int main()
 {
-
 	memset(visited, true, sizeof(visited));
 	int maze[N][M] = {
 		{ 1, 0, 1, 1, 0 },
@@ -93,11 +71,8 @@ int main()
 	fx = 2;
 	fy = 3;
 
-	if (isReachable(maze)) {
-		cout << "Path Found!" << '\n';
-	}
-	else
-		cout << "No Path Found!" << '\n';
-		
+	if (isReachable(maze)) 		cout << "Path Found!" << '\n';
+	else 	cout << "No Path Found!" << '\n';
+
 	return 0;
 }
