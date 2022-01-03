ef6ac38463ac373caeee3e0724a3e9e1261c9db8
nervaishere <nervaishere@github.com>
2022-01-03 17:15:24 +0330

    remove

--- System/Person.cpp
+++
@@ -1,5 +0,0 @@
-#include "Person.h"
-Person::Person(string n, string l, string nin, int a, Date B) :Name(n),Lastname(l),Nin(nin),Age(a), Birthday(B){
-    cout << "hi " << Name << " " << Lastname << endl;
-    cout << "You are " << Age << " years old and " << Birthday.Day << endl;
-}
